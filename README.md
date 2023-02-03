9a385ff00415941ee9ba601bd373b7339022706b
Anita Luri <anitaluri.h@gmail.com>
2023-02-03 15:23:20 -0300

    conversor de distancias

---
+++ README.md
@@ -0,0 +1,6 @@
+<body>
+  <h1> Conversor </h1>
+
+  <p> Projeto baseado no primeiro desafio da Imersão Dev 6ª Edição</p>
+   
+</body>
--- js/distance.js
+++ js/distance.js
@@ -1,14 +1,25 @@
-const api = "https://api.exchangerate-api.com/v4/latest/USD";
-
 const valueBox = document.querySelector('.value-box');
 const convert = document.querySelector('.convert');
 const fromCurrecy = document.querySelector(".from");
 const toCurrecy = document.querySelector(".to");
-const result = document.querySelector('.result');
-// const amount = document.getElementById('amount');
+const result = document.querySelector('.value-result');
 let valueFrom ;
 let resultFrom ; 
 let resultTo ;
+
+class distance {
+  constructor() {
+    this.MM = 1000000;
+    this.CM = 100000;
+    this.M = 1000;
+    this.KM = 1;
+    this.IN = 39370.1;
+    this.FT = 3280.84;
+    this.YD = 1093.61;
+    this.MI = 0.6213711922;
+    this.ANOLUZ = 0.0000000000001057;
+  }
+}
 
 valueBox.addEventListener('input', updateValue);
 function updateValue(event) {
@@ -25,19 +36,13 @@ toCurrecy.addEventListener('change', (event) => {
 });
 
 convert.addEventListener("click", getResults);
+
 function getResults() {
-    fetch(api)
-        .then(currency => {
-            return currency.json();
-        }).then(displayResults)
-        .catch(function(){
-          alert('erro')
-        })
+  const units = new distance()
+  if (resultTo == 'ANOLUZ' || resultFrom == 'ANOLUZ') {
+    result.innerHTML = ((units[resultTo]/ units[resultFrom])* valueFrom).toFixed(20) + " (aproximadamente)";
+  } else {
+    result.innerHTML = ((units[resultTo]/ units[resultFrom])* valueFrom).toFixed(6);
+  }
 }
 
-function displayResults(currency) {
-    let fromRate = currency.rates[resultFrom];
-    let toRate = currency.rates[resultTo];
-    result.innerHTML = 
-       ((toRate / fromRate) * valueFrom).toFixed(2);
-}
